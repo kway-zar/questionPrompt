b4f41b21e353aa8a764f94b75240e6d10dcae8ef
kway-zar <137285693+kway-zar@users.noreply.github.com>
2024-03-06 21:27:56 +0800

    patch

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ NO_BUTTON.addEventListener("click", MOVE_NO_BUTTON);
 
 function MOVE_NO_BUTTON() {
     
-    var x = Math.floor(Math.random()* w/4)-1;
+    var x = Math.floor(Math.random()* w/4);
     var y = Math.floor(Math.random()* h);
 
     if(y > 150) {
 
         console.log(true);
 
-        y = Math.sqrt(y);
+        y = Math.sqrt(y) *0.5;
+        y = Math.pow(y,2);
     }
     
     console.log("x:" + x);
