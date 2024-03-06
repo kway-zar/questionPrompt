7ef9f0a21dfbee8265f40416601443169d582f83
kway-zar <137285693+kway-zar@users.noreply.github.com>
2024-03-06 21:11:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,31 +2,28 @@ const NO_BUTTON = document.getElementById('no');
 var w = Math.floor(window.innerWidth);
 var h = Math.floor(window.innerHeight);
 
-console.log(h/3);
 
 NO_BUTTON.addEventListener("mouseover", MOVE_NO_BUTTON);
 NO_BUTTON.addEventListener("click", MOVE_NO_BUTTON);
 
-// var rect = NO_BUTTON.getBoundingClientRect();
-// var Y_AXIS = rect.y;
-// console.log("Y-AXIS:"+Y_AXIS);
-
-
 
 function MOVE_NO_BUTTON() {
     
-    var x = Math.floor(Math.random()* w);
+    var x = Math.floor(Math.random()* w/4)-1;
     var y = Math.floor(Math.random()* h);
 
-    // var rect = NO_BUTTON.getBoundingClientRect();
-    // var Y_AXIS = rect.y;
-    // console.log("Y-AXIS:"+Y_AXIS);
+    if(y > 150) {
+
+        console.log(true);
+
+        y = Math.sqrt(y);
+    }
+    
     console.log("x:" + x);
     console.log("y:" + y);
 
     NO_BUTTON.style.transform = "translate(" + x + "px," + y +"px)";
 
-    
-    
+      
 
 }
