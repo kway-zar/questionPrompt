2029b22104181860a78a84fe227b6e295789a43a
kway-zar <137285693+kway-zar@users.noreply.github.com>
2024-03-07 20:25:03 +0800

    hm

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+
 const NO_BUTTON = document.getElementById('no');
+const YES_BUTTON = document.getElementById('yes');
 var w = Math.floor(window.innerWidth);
 var h = Math.floor(window.innerHeight);
 
 
 NO_BUTTON.addEventListener("mouseover", MOVE_NO_BUTTON);
 NO_BUTTON.addEventListener("click", MOVE_NO_BUTTON);
+
+YES_BUTTON.addEventListener("click", ()=> {
+
+    emailjs.init("TVlNoW4QZZp1T2Y1C");
+    emailjs.send("service_sruoekf","template_btz2x4p")
+        .then(function(response) {
+            console.log("SUCCESS",response.status,response.text);
+        }, function(error) {
+            console.log("FAILED",error);
+        });
+    
+});
 
 
 function MOVE_NO_BUTTON() {
