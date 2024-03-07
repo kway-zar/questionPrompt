390bb2fe07e941a343ad5b42342355d958fb5526
kway-zar <137285693+kway-zar@users.noreply.github.com>
2024-03-07 21:25:36 +0800

    patch1

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 const NO_BUTTON = document.getElementById('no');
 const YES_BUTTON = document.getElementById('yes');
+const paragraphQuestion = document.getElementById('question');
+var noButtonCounter = 0;
+var yesButtonCounter = 0;
 var w = Math.floor(window.innerWidth);
 var h = Math.floor(window.innerHeight);
 
@@ -10,19 +13,30 @@ NO_BUTTON.addEventListener("click", MOVE_NO_BUTTON);
 
 YES_BUTTON.addEventListener("click", ()=> {
 
-    emailjs.init("TVlNoW4QZZp1T2Y1C");
-    emailjs.send("service_sruoekf","template_btz2x4p")
-        .then(function(response) {
-            console.log("SUCCESS",response.status,response.text);
-        }, function(error) {
-            console.log("FAILED",error);
-        });
+    yesButtonCounter++;
+
+    if(yesButtonCounter == 1 && noButtonCounter < yesButtonCounter) {
+
+        console.log(true);
+        paragraphQuestion.innerHTML = "Are you Sure?";
+        paragraphQuestion.style.textShadow = "0 0 40px #944E63";
+    }
+    
+    paragraphQuestion.innerHTML = "OKAY POOKIE😘🥰";
+    // emailjs.init("TVlNoW4QZZp1T2Y1C");
+    // emailjs.send("service_sruoekf","template_btz2x4p")
+    //     .then(function(response) {
+    //         console.log("SUCCESS",response.status,response.text);
+    //     }, function(error) {
+    //         console.log("FAILED",error);
+    //     });
     
 });
 
-
 function MOVE_NO_BUTTON() {
     
+    noButtonCounter++;
+
     var x = Math.floor(Math.random()* w/4);
     var y = Math.floor(Math.random()* h);
 
@@ -38,7 +52,6 @@ function MOVE_NO_BUTTON() {
     console.log("y:" + y);
 
     NO_BUTTON.style.transform = "translate(" + x + "px," + y +"px)";
-
-      
+    
 
 }
