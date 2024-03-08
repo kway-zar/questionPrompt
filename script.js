41795b20cac94f1284352470d01374a728157d5d
kway-zar <137285693+kway-zar@users.noreply.github.com>
2024-03-08 13:52:55 +0800

    PATCH

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 
 const NO_BUTTON = document.getElementById('no');
 const YES_BUTTON = document.getElementById('yes');
-const paragraphQuestion = document.getElementById('question');
+const questionElement = document.getElementById('question');
+const AuthorStatement = document.getElementById('AuthorStatement');
+
+const QUESTION = ["Are you my pookie!??", "Still No??","I beg you🥺", "One more NO and I'll-","...shut up now😔"];
+
+
 var noButtonCounter = 0;
 var yesButtonCounter = 0;
 var w = Math.floor(window.innerWidth);
 var h = Math.floor(window.innerHeight);
 
 
-NO_BUTTON.addEventListener("mouseover", MOVE_NO_BUTTON);
-NO_BUTTON.addEventListener("click", MOVE_NO_BUTTON);
+
+NO_BUTTON.addEventListener("mouseover",MOVE_NO_BUTTON);
+NO_BUTTON.addEventListener("click",MOVE_NO_BUTTON);
 
 YES_BUTTON.addEventListener("click", ()=> {
 
     yesButtonCounter++;
-
     if(yesButtonCounter == 1 && noButtonCounter < yesButtonCounter) {
 
         console.log(true);
-        paragraphQuestion.innerHTML = "Are you Sure?";
-        paragraphQuestion.style.textShadow = "0 0 40px #944E63";
+        AuthorStatement.innerHTML = "Are you Sure?";
+        AuthorStatement.style.textShadow = "0 0 40px #944E63";
+        
+    } else if(noButtonCounter> yesButtonCounter && yesButtonCounter == 1) {
+
+        ENDSession();
+
+    } else if(yesButtonCounter > 1) {
+        ENDSession();
     }
     
-    paragraphQuestion.innerHTML = "OKAY POOKIE😘🥰";
     // emailjs.init("TVlNoW4QZZp1T2Y1C");
     // emailjs.send("service_sruoekf","template_btz2x4p")
     //     .then(function(response) {
@@ -33,9 +44,12 @@ YES_BUTTON.addEventListener("click", ()=> {
     
 });
 
+
+
 function MOVE_NO_BUTTON() {
     
     noButtonCounter++;
+    console.log(noButtonCounter);
 
     var x = Math.floor(Math.random()* w/4);
     var y = Math.floor(Math.random()* h);
@@ -52,6 +66,26 @@ function MOVE_NO_BUTTON() {
     console.log("y:" + y);
 
     NO_BUTTON.style.transform = "translate(" + x + "px," + y +"px)";
-    
+    switch(noButtonCounter) {
 
+        case 1: questionElement.innerHTML = QUESTION[0];
+            break;
+        case 2: questionElement.innerHTML = QUESTION[1];
+            break;
+        case 3: questionElement.innerHTML = QUESTION[2];
+            break;
+        case 4: questionElement.innerHTML = QUESTION[3];
+            break;
+        case 5: questionElement.innerHTML = QUESTION[4];
+                AuthorStatement.style.opacity = "1";
+                AuthorStatement.innerHTML = "Just try to hold 'Yes' and tap it again🤭";
+                YES_BUTTON.style.animation = "1s ease-in-out 0.3s infinite alternate bounce";
+            break;
+    }
+
+}
+
+function ENDSession() {
+    questionElement.innerHTML = QUESTION[0];
+    AuthorStatement.innerHTML = "OKAY POOKIE😘🥰";
 }
