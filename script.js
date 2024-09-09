f06e219e5ee7b49b4a1d0f7cf7eac239eefd48db
kway-zar <137285693+kway-zar@users.noreply.github.com>
2024-09-09 19:45:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function MOVE_NO_BUTTON() {
     console.log(noButtonCounter);
 
     var x = Math.floor(Math.random()* w/4);
-    var y = Math.floor(Math.random()* h/3;
+    var y = Math.floor(Math.random()* h/3);
 
     if(y > 150) {
 
